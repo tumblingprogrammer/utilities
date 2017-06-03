231945224c5eb5adaeabde8780720b77f055aa68
Jose Pumar <jpumar@hotmail.com>
2017-06-03 08:30:53 -0700

    finalize and test media and static file serving

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from manage import get_env_variable
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls')),
 ]
+
+DJANGO_EXECUTION_ENVIRONMENT = get_env_variable('DJANGO_EXECUTION_ENVIRONMENT')
+if DJANGO_EXECUTION_ENVIRONMENT == 'LOCAL':
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
